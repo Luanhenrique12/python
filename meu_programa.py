b35807d7055a673335d81d7322e2abb7ad2c4f3c
Luanhenrique12 <hluan8867@gmail.com>
2025-05-07 15:50:56 -0300

    Meu primeiro projeto

---
+++ meu_programa.py
@@ -0,0 +1,65 @@
+print("Olá Mundo!")
+print(1500+(1500*5))
+
+
+
+def calculo(salario=1500, aumento=5):
+    print(salario+(salario*(aumento)/100))
+
+print(10+20*30)
+print(4**2+30)
+print((9**4+2)*6-1)
+
+
+
+
+salario = 1000
+aumento = 656278
+
+print(salario * aumento/100)
+
+nome = "Luan"
+produto = "Parabéns por garantir o produto"
+produto = 1999,99
+deconto = 200
+
+
+a=4
+b=10
+c=5.0
+d=1
+e=5
+print(a==c)
+print(a<b)
+print(d>b)
+print(c!=e)
+print(a==b)
+print(c<d)
+print(b>a)
+print(c>=e)
+print(e>=c)
+print(c<=c)
+print(c<=e)
+
+
+
+if a==c:
+    print("true")
+else:
+    print("false")
+
+if a<b:
+    print("true")
+else:
+    print("false")
+
+
+
+
+nome= "Luan Henrique"
+  
+print(len("luan"))
+print(nome.startswith("nrique"))
+print("Luan Henrique" .find("nrique"))
+y="this is lazy\t\n"
+print(y.strip())
